05dae9555f9015e25a8efd681958a03ee44594af
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-03-03 16:30:40 +0100

    import only occurrences since 1991

--- R/import_data.R
+++ R/import_data.R
@@ -1,14 +1,15 @@
 #' Import and standardise the raw data
 #' @param belgium path to the CSV file with the Belgian data.
 #' @param output path to the root of the data package
+#' @inheritParams git2rdata::write_vc
 #' @export
 #' @importFrom assertthat assert_that has_name is.string
 #' @importFrom git2rdata repository write_vc
-#' @importFrom dplyr bind_cols distinct mutate transmute %>%
+#' @importFrom dplyr bind_cols distinct filter mutate transmute %>%
 #' @importFrom readr locale read_delim
 #' @importFrom rlang .data
 #' @importFrom sf st_as_sf st_coordinates st_transform
-import_data <- function(belgium, output) {
+import_data <- function(belgium, output, strict = TRUE) {
   assert_that(is.string(belgium))
   root <- repository(output)
   raw_belgium <- read_delim(belgium, delim = ";", locale = locale())
@@ -34,6 +35,7 @@ import_data <- function(belgium, output) {
       location = .data$utm1km, long = .data$X, lat = .data$Y
     ) -> raw_belgium
   raw_belgium %>%
+    filter(.data$year > 1990) %>%
     mutate(
       location = factor(.data$location),
       taxon_key = factor(.data$taxon_key)
@@ -41,17 +43,19 @@ import_data <- function(belgium, output) {
   raw_data %>%
     distinct(.data$taxon_key, .data$scientific_name) %>%
     write_vc(
-      file = "inst/species", root = root, sorting = "taxon_key", stage = TRUE
+      file = "inst/species", root = root, sorting = "taxon_key", stage = TRUE,
+      strict = strict
     )
   raw_data %>%
     distinct(.data$country, .data$location, .data$long, .data$lat) %>%
     write_vc(
-      file = "inst/location", root = root, sorting = "location", stage = TRUE
+      file = "inst/location", root = root, sorting = "location", stage = TRUE,
+      strict = strict
     )
   raw_data %>%
     distinct(location = factor(.data$location), .data$year, .data$taxon_key) %>%
     write_vc(
-      file = "inst/occurrence", root = root,
+      file = "inst/occurrence", root = root, strict = strict,
       sorting = c("location", "year", "taxon_key"), stage = TRUE
     )
 }
