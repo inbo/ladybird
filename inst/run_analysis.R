05f0fe602497bc5499adcdb1a8a9f006202b6c10
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-04-13 19:31:04 +0200

    run analysis by country

--- inst/run_analysis.R
+++ inst/run_analysis.R
@@ -4,87 +4,165 @@ min_occurrences <- 1000
 min_species <- 3
 output_dir <- here("..", "Analysis", "models", "base")
 dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
+country <- "BE"
 ds <- load_relevant(
-  min_occurrences = min_occurrences, min_species = min_species
+  min_occurrences = min_occurrences, min_species = min_species,
+  country = country
 )
 available_species <- head(tail(colnames(ds), -2), -1)
+rm(ds)
+gc()
 
-for (country in c("BE", "NL")) {
-  for (species in available_species) {
-    output <- sprintf(
-      "%s/%s_%s_%i_%i.rds", output_dir, tolower(species), tolower(country),
-      min_occurrences, min_species
-    )
-    if (file.exists(output)) {
-      next
-    }
-    message("base: ", country, " ", species, " ", Sys.time())
-    bm <- try(base_model(
-      species = species, min_occurrences = min_occurrences, country = country,
-      min_species = min_species, knots = c(1990, 2000, 2010, 2020)
-    ))
-    if (!inherits(bm, "try-error")) {
-      saveRDS(bm, output)
-    }
+for (species in available_species) {
+  output <- sprintf(
+    "%s/%s_%s_%i_%i.rds", output_dir, tolower(species), tolower(country),
+    min_occurrences, min_species
+  )
+  if (file.exists(output)) {
+    next
   }
+  message("base: ", country, " ", species, " ", Sys.time())
+  bm <- try(base_model(
+    species = species, min_occurrences = min_occurrences, country = country,
+    min_species = min_species, knots = c(1990, 2000, 2010, 2020)
+  ))
+  if (!inherits(bm, "try-error")) {
+    saveRDS(bm, output)
+  }
+  rm(bm)
+  gc()
 }
 
-dir.create(file.path(output_dir, "..", "probability"), showWarnings = FALSE)
-for (country in c("BE", "NL")) {
-  secondary <- readRDS(
-    sprintf(
-      "%s/%s_%s_%i_%i.rds", output_dir, "harm_axyr", tolower(country),
-      min_occurrences, min_species
-    )
+dir.create(file.path(output_dir, "..", "probability_logit"), showWarnings = FALSE)
+secondary <- readRDS(
+  sprintf(
+    "%s/%s_%s_%i_%i.rds", output_dir, "harm_axyr", tolower(country),
+    min_occurrences, min_species
   )
-  for (species in available_species) {
-    if (species == "Harm_axyr") {
-      next
-    }
-    output <- sprintf(
-      "%s/%s_%s_%i_%i.rds", file.path(output_dir, "..", "probability"),
-      tolower(species), tolower(country), min_occurrences, min_species
-    )
-    if (file.exists(output)) {
-      next
-    }
-    message("probability: ", country, " ", species, " ", Sys.time())
-    sm <- try(probability_model(
-      species = species, min_occurrences = min_occurrences,
-      min_species = min_species, secondary = secondary,
-      knots = c(1990, 2000, 2010, 2020), country = country
-    ))
-    if (!inherits(sm, "try-error")) {
-      saveRDS(sm, output)
-    }
+)
+for (species in available_species) {
+  if (species == "Harm_axyr") {
+    next
   }
+  output <- sprintf(
+    "%s/%s_%s_%i_%i.rds", file.path(output_dir, "..", "probability_logit"),
+    tolower(species), tolower(country), min_occurrences, min_species
+  )
+  if (file.exists(output)) {
+    next
+  }
+  message("probability: ", country, " ", species, " ", Sys.time())
+  sm <- try(probability_model(
+    species = species, min_occurrences = min_occurrences,
+    min_species = min_species, secondary = secondary,
+    knots = c(1990, 2000, 2010, 2020), country = country
+  ))
+  if (!inherits(sm, "try-error")) {
+    saveRDS(sm, output)
+  }
+  rm(sm)
+  gc()
 }
 
 dir.create(file.path(output_dir, "..", "cumulative"), showWarnings = FALSE)
-for (country in c("BE", "NL")) {
-  secondary <- readRDS(
-    sprintf(
-      "%s/%s_%s_%i_%i.rds", output_dir, "harm_axyr", tolower(country),
-      min_occurrences, min_species
-    )
+for (species in available_species) {
+  if (species == "Harm_axyr") {
+    next
+  }
+  output <- sprintf(
+    "%s/%s_%s_%i_%i.rds", file.path(output_dir, "..", "cumulative"),
+    tolower(species), tolower(country), min_occurrences, min_species
   )
-  for (species in available_species) {
-    if (species == "Harm_axyr") {
-      next
-    }
-    output <- sprintf(
-      "%s/%s_%s_%i_%i.rds", file.path(output_dir, "..", "cumulative"),
-      tolower(species), tolower(country), min_occurrences, min_species
-    )
-    if (file.exists(output)) {
-      next
-    }
-    message("cumulative: ", country, " ", species, " ", Sys.time())
-    sm <- cumulative_model(
-      species = species, min_occurrences = min_occurrences,
-      min_species = min_species, secondary = secondary,
-      knots = c(1990, 2000, 2010, 2020), country = country
-    )
+  if (file.exists(output)) {
+    next
+  }
+  message("cumulative: ", country, " ", species, " ", Sys.time())
+  sm <- cumulative_model(
+    species = species, min_occurrences = min_occurrences,
+    min_species = min_species, secondary = secondary,
+    knots = c(1990, 2000, 2010, 2020), country = country
+  )
+  saveRDS(sm, output)
+  rm(sm)
+  gc()
+}
+
+
+country <- "NL"
+ds <- load_relevant(
+  min_occurrences = min_occurrences, min_species = min_species,
+  country = country
+)
+available_species <- head(tail(colnames(ds), -2), -1)
+
+for (species in available_species) {
+  output <- sprintf(
+    "%s/%s_%s_%i_%i.rds", output_dir, tolower(species), tolower(country),
+    min_occurrences, min_species
+  )
+  if (file.exists(output)) {
+    next
+  }
+  message("base: ", country, " ", species, " ", Sys.time())
+  bm <- try(base_model(
+    species = species, min_occurrences = min_occurrences, country = country,
+    min_species = min_species, knots = c(1990, 2000, 2010, 2020)
+  ))
+  if (!inherits(bm, "try-error")) {
+    saveRDS(bm, output)
+  }
+  rm(bm)
+  gc()
+}
+
+secondary <- readRDS(
+  sprintf(
+    "%s/%s_%s_%i_%i.rds", output_dir, "harm_axyr", tolower(country),
+    min_occurrences, min_species
+  )
+)
+for (species in available_species) {
+  if (species == "Harm_axyr") {
+    next
+  }
+  output <- sprintf(
+    "%s/%s_%s_%i_%i.rds", file.path(output_dir, "..", "probability_logit"),
+    tolower(species), tolower(country), min_occurrences, min_species
+  )
+  if (file.exists(output)) {
+    next
+  }
+  message("probability: ", country, " ", species, " ", Sys.time())
+  sm <- try(probability_model(
+    species = species, min_occurrences = min_occurrences,
+    min_species = min_species, secondary = secondary,
+    knots = c(1990, 2000, 2010, 2020), country = country
+  ))
+  if (!inherits(sm, "try-error")) {
     saveRDS(sm, output)
   }
+  rm(sm)
+  gc()
+}
+
+for (species in available_species) {
+  if (species == "Harm_axyr") {
+    next
+  }
+  output <- sprintf(
+    "%s/%s_%s_%i_%i.rds", file.path(output_dir, "..", "cumulative"),
+    tolower(species), tolower(country), min_occurrences, min_species
+  )
+  if (file.exists(output)) {
+    next
+  }
+  message("cumulative: ", country, " ", species, " ", Sys.time())
+  sm <- cumulative_model(
+    species = species, min_occurrences = min_occurrences,
+    min_species = min_species, secondary = secondary,
+    knots = c(1990, 2000, 2010, 2020), country = country
+  )
+  saveRDS(sm, output)
+  rm(sm)
+  gc()
 }
