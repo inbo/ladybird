78b2b3febb274d69bd33315e61b1e0da76ad9f08
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-04-30 14:48:45 +0200

    import data from Brittain

--- R/import_data.R
+++ R/import_data.R
@@ -10,33 +10,32 @@
 #' @export
 #' @importFrom assertthat assert_that has_name is.string
 #' @importFrom git2rdata repository write_vc
-#' @importFrom dplyr anti_join bind_cols distinct filter mutate semi_join
+#' @importFrom dplyr across anti_join bind_cols distinct filter mutate semi_join
 #' transmute %>%
 #' @importFrom readr locale read_delim
 #' @importFrom rlang .data
 #' @importFrom sf st_as_sf st_coordinates st_transform
 import_data <- function(
-  belgium_occurrence, belgium_visits,
-  netherlands_occurrence, netherlands_visits,
-  output, strict = TRUE
+  belgium_occurrence, belgium_visits, netherlands_occurrence,
+  netherlands_visits, gb_occurrence, gb_visits, output, strict = TRUE
 ) {
   assert_that(is.string(belgium_occurrence), is.string(belgium_visits))
   assert_that(is.string(netherlands_occurrence), is.string(netherlands_visits))
+  assert_that(is.string(gb_occurrence), is.string(gb_visits))
   root <- repository(output)
+
   raw_belgium <- read_delim(belgium_occurrence, delim = ";", locale = locale())
   assert_that(
-    all(
-      has_name(
-        raw_belgium,
-        c("taxon_key", "scientific_name", "year", "utm1km", "centroid_x",
-          "centroid_y")
-      )
+    has_name(
+      raw_belgium,
+      c("taxon_key", "scientific_name", "year", "utm1km", "centroid_x",
+        "centroid_y")
     )
   )
   raw_bel_visit <- read_delim(belgium_visits, delim = ";", locale = locale())
   assert_that(all(has_name(raw_bel_visit, c("year", "utm1km", "nDayVisits"))))
   raw_belgium %>%
-    filter(.data$year >= 1991) %>%
+    filter(.data$year >= 1990) %>%
     distinct(.data$year, .data$utm1km) %>%
     anti_join(raw_bel_visit, by = c("year", "utm1km")) %>%
     nrow() -> missing_visits
@@ -49,24 +48,44 @@ import_data <- function(
     netherlands_occurrence, delim = ";", locale = locale()
   )
   assert_that(
-    all(
-      has_name(
-        raw_netherlands,
-        c("taxonKey", "scientific_name", "year", "utm1km", "centroid_x",
-          "centroid_y")
-      )
+    has_name(
+      raw_netherlands,
+      c("taxon_key", "scientific_name", "year", "utm1km", "centroid_x",
+        "centroid_y")
     )
   )
   raw_nl_visit <- read_delim(netherlands_visits, delim = ";", locale = locale())
   assert_that(all(has_name(raw_nl_visit, c("year", "utm1km", "nDayVisits"))))
+  raw_netherlands  %>%
+    mutate(utm1km = sprintf("%06i", .data$utm1km)) -> raw_netherlands
   raw_netherlands %>%
-    filter(.data$year >= 1991) %>%
+    filter(.data$year >= 1990) %>%
     distinct(.data$year, .data$utm1km) %>%
     anti_join(raw_nl_visit, by = c("year", "utm1km")) %>%
     nrow() -> missing_visits
   assert_that(
     missing_visits == 0,
     msg = "Some Dutch occurrence data have no matching number of visits."
+  )
+
+  raw_gb <- read_delim(gb_occurrence, delim = ";", locale = locale())
+  assert_that(
+    has_name(
+      raw_gb,
+      c("taxon_key", "scientific_name", "year", "utm1km", "centroid_x",
+        "centroid_y")
+    )
+  )
+  raw_gb_visit <- read_delim(gb_visits, delim = ";", locale = locale())
+  assert_that(all(has_name(raw_gb_visit, c("year", "utm1km", "nDayVisits"))))
+  raw_gb %>%
+    filter(.data$year >= 1990) %>%
+    distinct(.data$year, .data$utm1km) %>%
+    anti_join(raw_gb_visit, by = c("year", "utm1km")) %>%
+    nrow() -> missing_visits
+  assert_that(
+    missing_visits == 0,
+    msg = "Some British occurrence data have no matching number of visits."
   )
 
   st_as_sf(
@@ -94,19 +113,31 @@ import_data <- function(
     as.data.frame() %>%
     bind_cols(raw_netherlands) %>%
     transmute(
-      taxon_key = .data$taxonKey, .data$scientific_name, .data$year,
+      .data$taxon_key, .data$scientific_name, .data$year,
       country = factor("NL", levels = c("BE", "NL", "GB")),
       location = .data$utm1km, long = .data$X, lat = .data$Y
     ) -> raw_netherlands
+  st_as_sf(
+    raw_gb, coords = c("centroid_x", "centroid_y"), crs = 27700
+  ) %>%
+    st_transform(crs = 4326) %>%
+    st_coordinates() %>%
+    as.data.frame() %>%
+    bind_cols(raw_gb) %>%
+    transmute(
+      .data$taxon_key, .data$scientific_name, .data$year,
+      country = factor("GB", levels = c("BE", "NL", "GB")),
+      location = .data$utm1km, long = .data$X, lat = .data$Y
+    ) -> raw_gb
   raw_belgium %>%
-    bind_rows(raw_netherlands) %>%
-    filter(.data$year >= 1991) %>%
+    bind_rows(raw_netherlands, raw_gb) %>%
+    filter(.data$year >= 1990) %>%
     mutate(
       location = factor(.data$location),
       taxon_key = factor(.data$taxon_key)
     ) -> raw_data
   raw_bel_visit %>%
-    bind_rows(raw_nl_visit) %>%
+    bind_rows(raw_nl_visit, raw_gb_visit) %>%
     transmute(
       .data$year,
       location = factor(.data$utm1km, levels = levels(raw_data$location)),
@@ -118,13 +149,6 @@ import_data <- function(
       strict = strict
     )
   raw_data %>%
-    mutate(
-      scientific_name = ifelse(
-        .data$scientific_name == "Harmonia axyridisHarmonia axyridis",
-        "Harmonia axyridis",
-        .data$scientific_name
-      )
-    ) %>%
     distinct(.data$taxon_key, .data$scientific_name) %>%
     mutate(
       code = gsub("^(.{4}).* (.{4}).*$", "\\1_\\2", .data$scientific_name)
@@ -135,6 +159,7 @@ import_data <- function(
     )
   raw_data %>%
     distinct(.data$country, .data$location, .data$long, .data$lat) %>%
+    mutate(across(c(.data$long, .data$lat), round, digits = 4)) %>%
     write_vc(
       file = "inst/location", root = root, sorting = "location", stage = TRUE,
       strict = strict
@@ -146,4 +171,3 @@ import_data <- function(
       sorting = c("location", "year", "taxon_key"), stage = TRUE
     )
 }
-
--- inst/import_script.R
+++ inst/import_script.R
@@ -4,5 +4,7 @@ import_data(
   belgium_visits = "../Data/NDayVisits_datasetBelgium.csv",
   netherlands_occurrence = "../Data/datasetNetherlandsSeason.csv",
   netherlands_visits = "../Data/NDayVisits_datasetNetherlands.csv",
+  gb_occurrence = "../Data/datasetBritainSeason.csv",
+  gb_visits = "../Data/NDayVisits_datasetBritain.csv",
   output = "."
 )
